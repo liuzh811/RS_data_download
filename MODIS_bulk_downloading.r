94089de7a90608b63b2a36719fd0545bf6295610
liuzh811 <liuzh811@gmail.com>
2016-04-27 11:40:45 -0500

    Update MODIS_bulk_downloading.r

--- MODIS_bulk_downloading.r
+++ MODIS_bulk_downloading.r
@@ -17,8 +17,12 @@ dates <- as.POSIXct( as.Date(c("1/1/2000","30/7/2015"),format = "%d/%m/%Y") )
 dates2 <- transDate(dates[1],dates[2]) # Transform input dates from before
 proj.geo = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0 "
 
+product="MCD43A4"
+collection = getCollection(product, collection=NULL, newest=TRUE, forceCheck=FALSE, as="character", quiet=TRUE)
+
 #download reflectance
-runGdal(product="MCD43A4",  #Nadir BRDF-Adjusted Reflectance, 1000 m reso
+runGdal(product=product,  #Nadir BRDF-Adjusted Reflectance, 1000 m reso
+        collection = collection,
         begin=dates2$beginDOY,
         end = dates2$endDOY,
         tileH = 16:18,tileV = 7:8,
